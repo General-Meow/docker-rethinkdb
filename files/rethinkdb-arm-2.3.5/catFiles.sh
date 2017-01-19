66ebc1d50e19857e8a63b8e11b54f4e78e8a747b
General-Meow <spl.ivalis@gmail.com>
2017-01-19 23:20:59 +0000

    docker file arm fix

--- files/rethinkdb-arm-2.3.5/catFiles.sh
+++ files/rethinkdb-arm-2.3.5/catFiles.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cat /home/rethinkdb/files/rethink-arm-2.3.5/rethinkdb-arm-2.3.5.tar.gz.* > rethinkdb.tar.gz
+cat /home/rethinkdb/files/rethinkdb-arm-2.3.5/rethinkdb-arm-2.3.5.tar.gz.* > rethinkdb.tar.gz
 tar -zxvf /home/rethinkdb/files/rethinkdb-arm-2.3.5/rethinkdb.tar.gz -C /home/rethinkdb
