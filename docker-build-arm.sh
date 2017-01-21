dd831874d64dc5d512ded3140ea6bd0ac5dca9ff
General-Meow <spl.ivalis@gmail.com>
2017-01-21 21:59:25 +0000

    docker file arm fix

--- docker-build-arm.sh
+++ docker-build-arm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cat ./files/rethinkdb-arm-2.3.5/rethinkdb-arm-2.3.5.tar.gz.* > ./rethinkdb.tar.gz
-docker build -t generalmeow/rethinkdb:1.0-arm -f Dockerfile-arm .
+tar -zxvf ./rethinkdb.tar.gz
 rm ./rethinkdb.tar.gz
+docker build -t generalmeow/rethinkdb:1.0-arm -f Dockerfile-arm .
